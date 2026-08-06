8d4704b4cf069647a34067b1fe8e19764d8c8cce
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2026-08-06 23:59:43 +0100

    chore: add env var

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from ai-agent!")
+import os
+from dotenv import load_dotenv
+
+
+def load_env_variable() -> str:
+    """Loads an environment variable or raises a RuntimeError if missing"""
+    load_dotenv()
+    api_key = os.getenv("OPENROUTER_API_KEY")
+    if api_key is None:
+        raise RuntimeError(
+            "OPENROUTER_API_KEY environment variable is missing")
+    return api_key
+
+
+def main() -> None:
+    api_key = load_env_variable()
+    print(f"Loaded API Key: {api_key}")
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except RuntimeError as e:
+        print(f"Error: {e}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
